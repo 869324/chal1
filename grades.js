7cef88343991d4a6bdb78bf12d3894c177847420
unknown <javan.kyalo@thejitu.com>
2022-11-26 12:03:44 +0300

    chal1

---
+++ grades.js
@@ -0,0 +1,23 @@
+function submit() {
+  const marks = document.getElementById("marks").value;
+  if (marks >= 0 && marks <= 100) {
+    let grade;
+
+    if (marks > 79) {
+      grade = "A";
+    } else if (marks >= 60) {
+      grade = "B";
+    } else if (marks >= 49) {
+      grade = "C";
+    } else if (marks >= 40) {
+      grade = "D";
+    } else {
+      grade = "E";
+    }
+
+    const output = document.getElementById("grade");
+    output.textContent = grade;
+  } else {
+    alert("Invalid input");
+  }
+}
